c59c62aed7ff3e0229532b27dcab7b23ddc36b15
Branko Vukelic <branko@brankovukelic.com>
2015-05-01 12:02:51 +0200

    Move values to a common module

--- outernet_metadata/validate.py
+++ outernet_metadata/validate.py
@@ -12,48 +12,21 @@ file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 
 from __future__ import print_function
 
-import re
 import sys
 import json
 
 import validators as v
+
+from . import values
 
 try:
     FILE_ERRORS = (FileNotFoundError, OSError)
 except NameError:
     FILE_ERRORS = (OSError,)
 
-PLACEHOLDER_RE = re.compile(r'^\$[A-Z]+$')
-LOCALE_RE = re.compile(r'^[a-z]{2}([_-][a-zA-Z]+)?$', re.I)
-COMMASEP_RE = re.compile(r'^[\w ]+(?:, ?[\w ]+)*$', re.U)
-RELPATH_RE = re.compile(r'^[^/]+(/[^/]+)*$')
-TS_FMT = '%Y-%m-%d %H:%M:%S UTC'
-DATE_FMT = '%Y-%m-%d'
-LICENSES = ('CC-BY', 'CC-BY-ND', 'CC-BY-NC', 'CC-BY-ND-NC', 'CC-BY-SA',
-            'CC-BY-NC-SA', 'GFDL', 'OPL', 'OCL', 'ADL', 'FAL', 'PD', 'OF',
-            'ARL', 'ON')
 
-SPECS = {
-    'title': [v.required, v.nonempty],
-    'url': [v.required, v.nonempty, v.url],
-    'timestamp': [v.required, v.nonempty, v.timestamp(TS_FMT)],
-    'broadcast': [v.required, v.nonempty,
-                  v.OR(v.timestamp(DATE_FMT), v.match(PLACEHOLDER_RE))],
-    'license': [v.required, v.isin(LICENSES)],
-    'images': [v.optional(), v.istype(int), v.gte(0)],
-    'language': [v.optional(''), v.nonempty, v.match(LOCALE_RE)],
-    'multipage': [v.optional(), v.istype(bool)],
-    'index': [v.optional(''), v.match(RELPATH_RE)],
-    'keywords': [v.optional(''), v.nonempty, v.match(COMMASEP_RE)],
-    'archive': [v.optional(''), v.nonempty],
-    'partner': [v.optional(''), v.nonempty],
-    'publisher': [v.optional(''), v.nonempty],
-    'is_partner': [v.optional(), v.istype(bool)],
-    'is_sponsored': [v.optional(), v.istype(bool)],
-    'keep_formatting': [v.optional(), v.istype(bool)],
-}
-
-VALIDATOR = v.spec_validator(SPECS, key=lambda k: lambda obj: obj.get(k))
+VALIDATOR = v.spec_validator(values.SPECS,
+                             key=lambda k: lambda obj: obj.get(k))
 
 
 def load(path):
