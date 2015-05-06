a3ea9e9825ebf1b0a5ccc5e2819ef754910af63a
Branko Vukelic <branko@brankovukelic.com>
2015-05-06 11:59:04 +0200

    Fixed project link

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     description='Library for working with Outernet metadata',
     license='GPLv3',
     keywords='json, validation, templates, metadata, outernet',
-    url='https://github.com/Outernet-Project/bottle-fdsend',
+    url='https://github.com/Outernet-Project/outernet-metadata',
     packages=find_packages(),
     long_description=read('README.rst'),
     install_requires=[
